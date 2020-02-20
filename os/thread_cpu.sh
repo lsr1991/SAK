09f737df212502088db52f0ca8cd4cd73f921c5e
Shurong Lin <shuronglin1991@163.com>
2020-02-20 19:03:00 +0800

    fix tid filter;add fixed width in output

--- os/thread_cpu.sh
+++ os/thread_cpu.sh
@@ -10,7 +10,7 @@ pid=$1
 tmpfile=$(mktemp)
 jstackfile=$tmpfile.j
 outfile=$tmpfile.out
-echo "cpu | thread name"> $outfile
+echo "cpu  | thread name"> $outfile
 top -b -n 1 -H -p $pid | tail -n +8 | sed -r 's/^\s+//g' | awk '{print $1","$9}' > $tmpfile
 jstack $pid > $jstackfile
 for i in $(cat $tmpfile)
@@ -18,8 +18,8 @@ do
     tid=$(echo $i | cut -d , -f1)
     cpu=$(echo $i | cut -d , -f2)
     tid_hex=$(echo "obase=16; $tid" | bc)
-    tname=$(grep -i $tid_hex $jstackfile | egrep -o '^".*"')
-    echo "$cpu | $tname" >> $outfile
+    tname=$(grep -i "0x$tid_hex" $jstackfile | egrep -o '^".*"')
+    printf '%-4s | %s\n' "$cpu" "$tname" >> $outfile
 done
 cat $outfile
 rm ${tmpfile}*
