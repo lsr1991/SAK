83cd0720ee208fa80f5dcd34a292201d05f121d0
Shurong Lin <shuronglin1991@163.com>
2020-02-20 16:16:12 +0800

    add thread_cpu.sh

---
+++ os/thread_cpu.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+usage="
+Usage: <pid>
+"
+if [ $# -lt 1 ]; then
+    echo "$usage"
+    exit 1
+fi
+pid=$1
+tmpfile=$(mktemp)
+jstackfile=$tmpfile.j
+outfile=$tmpfile.out
+echo "cpu | thread name"> $outfile
+top -b -n 1 -H -p $pid | tail -n +8 | sed -r 's/^\s+//g' | awk '{print $1","$9}' > $tmpfile
+jstack $pid > $jstackfile
+for i in $(cat $tmpfile)
+do
+    tid=$(echo $i | cut -d , -f1)
+    cpu=$(echo $i | cut -d , -f2)
+    tid_hex=$(echo "obase=16; $tid" | bc)
+    tname=$(grep -i $tid_hex $jstackfile | egrep -o '^".*"')
+    echo "$cpu | $tname" >> $outfile
+done
+cat $outfile
+rm ${tmpfile}*
